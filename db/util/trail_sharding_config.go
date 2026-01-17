b11597b47a5e804e176fb642eeacdda609b68d0a
Matthias Ullmann <matthias@familie-ullmann.ch>
2026-01-17 11:03:35 +0100

    fix gofmt

--- db/util/trail_sharding_config.go
+++ db/util/trail_sharding_config.go
@@ -1,8 +1,8 @@
 package util
 
 const (
-	TrailBucketMax = 900
-	QuadTreeMaxDepth  = 18
-	RootSplitLat      = 0.0
-	RootSplitLon      = -30.0
+	TrailBucketMax   = 900
+	QuadTreeMaxDepth = 18
+	RootSplitLat     = 0.0
+	RootSplitLon     = -30.0
 )
