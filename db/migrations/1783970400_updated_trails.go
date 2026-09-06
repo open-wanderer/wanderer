0e4c417358d3ec8b41127d72ba9d1aa224595645
Christian Beutel <>
2026-09-06 10:55:15 +0200

    fixes ruleset

--- db/migrations/1783970400_updated_trails.go
+++ db/migrations/1783970400_updated_trails.go
@@ -9,28 +9,28 @@ import (
 
 func init() {
 	m.Register(func(app core.App) error {
-		collection, err := app.FindCollectionByNameOrId("goeo2ubp103rzp9")
+		collection, err := app.FindCollectionByNameOrId("e864strfxo14pm4")
 		if err != nil {
 			return err
 		}
 
 		// update collection data
 		if err := json.Unmarshal([]byte(`{
-			"createRule": "@request.auth.id != \"\" && (trail.author.user = @request.auth.id || (trail.trail_share_via_trail.actor.user ?= @request.auth.id && trail.trail_share_via_trail.permission ?= \"edit\")) && @request.body.author.user = @request.auth.id"
+			"updateRule": "author.user = @request.auth.id || (@request.auth.id != \"\" && trail_share_via_trail.actor.user ?= @request.auth.id && trail_share_via_trail.permission ?= \"edit\")"
 		}`), &collection); err != nil {
 			return err
 		}
 
 		return app.Save(collection)
 	}, func(app core.App) error {
-		collection, err := app.FindCollectionByNameOrId("goeo2ubp103rzp9")
+		collection, err := app.FindCollectionByNameOrId("e864strfxo14pm4")
 		if err != nil {
 			return err
 		}
 
 		// update collection data
 		if err := json.Unmarshal([]byte(`{
-			"createRule": "@request.auth.id != \"\""
+			"updateRule": "author.user = @request.auth.id || (@request.auth.id != \"\" && trail_share_via_trail.trail = id && trail_share_via_trail.actor.user ?= @request.auth.id && trail_share_via_trail.permission = \"edit\")"
 		}`), &collection); err != nil {
 			return err
 		}
--- db/migrations/1783970500_updated_lists.go
+++ db/migrations/1783970500_updated_lists.go
@@ -9,28 +9,28 @@ import (
 
 func init() {
 	m.Register(func(app core.App) error {
-		collection, err := app.FindCollectionByNameOrId("goeo2ubp103rzp9")
+		collection, err := app.FindCollectionByNameOrId("r6gu2ajyidy1x69")
 		if err != nil {
 			return err
 		}
 
 		// update collection data
 		if err := json.Unmarshal([]byte(`{
-			"createRule": "@request.auth.id != \"\" && (trail.author.user = @request.auth.id || (trail.trail_share_via_trail.actor.user ?= @request.auth.id && trail.trail_share_via_trail.permission ?= \"edit\")) && @request.body.author.user = @request.auth.id"
+			"updateRule": "author.user = @request.auth.id || (@request.auth.id != \"\" && list_share_via_list.actor.user ?= @request.auth.id && list_share_via_list.permission ?= \"edit\")"
 		}`), &collection); err != nil {
 			return err
 		}
 
 		return app.Save(collection)
 	}, func(app core.App) error {
-		collection, err := app.FindCollectionByNameOrId("goeo2ubp103rzp9")
+		collection, err := app.FindCollectionByNameOrId("r6gu2ajyidy1x69")
 		if err != nil {
 			return err
 		}
 
 		// update collection data
 		if err := json.Unmarshal([]byte(`{
-			"createRule": "@request.auth.id != \"\""
+			"updateRule": "author.user = @request.auth.id || (@request.auth.id != \"\" && list_share_via_list.list = id && list_share_via_list.actor.user ?= @request.auth.id && list_share_via_list.permission = \"edit\")"
 		}`), &collection); err != nil {
 			return err
 		}
